810a185cb795d81a6fbf424ab754db1fb0dd3780
Kelechukwu <paparazzip8@gmail.com>
2025-01-17 19:39:28 +0100

    finished creating my basic NFT

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -5,18 +5,23 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-uint256 private s_tokenCounter;
-    constructor(string memory _name, string memory _symbol) ERC721("Dogie", "DOG") {
-     s_tokenCounter = 0;
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) s_tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
     }
 
-    function mintNft(address to) external {
-        _safeMint(to, s_tokenCounter);
+    function mintNft(string memory tokenUri) external {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked("https://token.com/", tokenId));
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
     }
 
     function transfer(address to, uint256 tokenId) external {
