17066f99fb0e45e085060fe2148519ae276d816f
Kelechukwu <paparazzip8@gmail.com>
2025-02-18 01:17:01 +0100

    modules

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+
+contract DeployMoodNft is Script {
+    function run() external {
+        vm.startBroadcast();
+        new MoodNft("sadSvgImageURI", "happySvgImageURI");
+        vm.stopBroadcast();
+    }
+}
