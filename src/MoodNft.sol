9865e929fa73df8929cf6a4d03ebba5a80013d78
Kelechukwu <paparazzip8@gmail.com>
2025-02-19 05:22:27 +0100

    completed test to check if encoded svg matches the expected URI

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,6 +6,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    error MoodNft__CantFlipMoodIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_sadSvgImageURI;
     string private s_happySvgImageURI;
@@ -30,6 +32,18 @@ contract MoodNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        //we only want the owner of the NFT to be able to call this method
+        if (!_isAuthorized(msg.sender, ownerOf(tokenId), tokenId)) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
