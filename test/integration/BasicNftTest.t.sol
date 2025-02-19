8090bd405bd30fd5ebb3ab8398bb9d174f3c93ee
Kelechukwu <paparazzip8@gmail.com>
2025-02-20 00:00:10 +0100

    wrote test for flipping image URI

--- test/integration/BasicNftTest.t.sol
+++ test/integration/BasicNftTest.t.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
-import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
-import {BasicNFT} from "../src/BasicNFT.sol";
+import {DeployBasicNft} from "../../script/DeployBasicNft.s.sol";
+import {BasicNFT} from "../../src/BasicNFT.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
